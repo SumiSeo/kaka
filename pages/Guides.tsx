b5d2066687259565ce2cf388ef02d9742ec25e3e
Sumi <qkobr94@gmail.com>
2023-01-18 13:37:25 +0100

    chore(mockup): add mockup html tag

--- pages/Guides.tsx
+++ pages/Guides.tsx
@@ -1,5 +1,15 @@
 const Guides = (): JSX.Element => {
-  return <div>Guides</div>;
+  return (
+    <div>
+      <h1>Guides</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non facere
+        maiores veniam quis similique modi, earum, explicabo soluta, deserunt
+        perferendis at commodi vel accusamus molestiae. Quo repellendus rem
+        consequuntur molestiae!
+      </p>
+    </div>
+  );
 };
 
 export default Guides;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,33 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <div>
+        <h1>Welcome home!!!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non facere
+          maiores veniam quis similique modi, earum, explicabo soluta, deserunt
+          perferendis at commodi vel accusamus molestiae. Quo repellendus rem
+          consequuntur molestiae!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non facere
+          maiores veniam quis similique modi, earum, explicabo soluta, deserunt
+          perferendis at commodi vel accusamus molestiae. Quo repellendus rem
+          consequuntur molestiae!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non facere
+          maiores veniam quis similique modi, earum, explicabo soluta, deserunt
+          perferendis at commodi vel accusamus molestiae. Quo repellendus rem
+          consequuntur molestiae!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non facere
+          maiores veniam quis similique modi, earum, explicabo soluta, deserunt
+          perferendis at commodi vel accusamus molestiae. Quo repellendus rem
+          consequuntur molestiae!
+        </p>
+      </div>
     </>
   );
 }
