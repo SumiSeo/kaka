d8b9a3aca0494e1ad2cd67029f928e09397ce4c9
Sumi <qkobr94@gmail.com>
2023-01-20 16:25:08 +0100

    feat(auth): create context and init netlify identity

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
+import { useContext } from "react";
+import AuthContext from "../stores/authContext";
 import styles from "@/styles/NavBar.module.scss";
 
 const NavBar = (): JSX.Element => {
+  const user = useContext(AuthContext);
+  console.log(user);
   return (
     <div className={styles.navigation}>
       <Link className={styles.navigation__list} href="/">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/base/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/comps/Layout";
+import { AuthContextProvider } from "@/stores/authContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthContextProvider>
   );
 }
