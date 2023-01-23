26c3ba78efa25326b4ba0ec273b8cf7fdcc946d2
Sumi <qkobr94@gmail.com>
2023-01-23 15:57:40 +0100

    feat(expenses): get user login information to protect pages

--- functions/expenses.js
+++ functions/expenses.js
@@ -1,1 +1,22 @@
-exports.handler = async (event, context) => {};
+exports.handler = async (event, context) => {
+  const guides = [
+    {
+      name: "sumi",
+      age: 28,
+    },
+    {
+      name: "yeji",
+      age: 28,
+    },
+  ];
+  if (context.clientContext.user) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(guides),
+    };
+  }
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ msg: "Sorry, you must be logged in to see this" }),
+  };
+};
