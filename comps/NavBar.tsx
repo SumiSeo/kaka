ef71adc353452a165d4de37603dcd954342970f8
Sumi <qkobr94@gmail.com>
2023-01-25 21:33:30 +0100

    feat: delete Netlify connection for Hasura configuration

--- comps/NavBar.tsx
+++ comps/NavBar.tsx
@@ -1,39 +1,24 @@
 import Link from "next/link";
-import { useContext } from "react";
-import AuthContext from "../stores/authContext";
+
 import styles from "@/styles/NavBar.module.scss";
 
 const NavBar = (): JSX.Element => {
-  const { user, login, logout, authReady } = useContext(AuthContext);
-  console.log(user, login);
   return (
     <div className={styles.navigation}>
-      {authReady && (
-        <ul>
-          {user == null ? (
-            <Link onClick={login} className={styles.navigation__list} href="/">
-              <span></span>
-            </Link>
-          ) : (
-            <Link
-              onClick={logout}
-              className={styles.navigation__logout}
-              href="/"
-            >
-              <span></span>
-            </Link>
-          )}
-          <Link className={styles.navigation__list} href="/Expenses">
-            <span></span>
-          </Link>
-          <Link className={styles.navigation__list} href="/Incomes">
-            <span></span>
-          </Link>
-          <Link className={styles.navigation__list} href="/Investment">
-            <span></span>
-          </Link>
-        </ul>
-      )}
+      <ul>
+        <Link className={styles.navigation__list} href="/">
+          <span></span>
+        </Link>
+        <Link className={styles.navigation__list} href="/Expenses">
+          <span></span>
+        </Link>
+        <Link className={styles.navigation__list} href="/Incomes">
+          <span></span>
+        </Link>
+        <Link className={styles.navigation__list} href="/Investment">
+          <span></span>
+        </Link>
+      </ul>
     </div>
   );
 };
--- pages/Expenses.tsx
+++ pages/Expenses.tsx
@@ -1,54 +1,7 @@
-import AuthContext from "@/stores/authContext";
-import { useContext, useEffect, useState } from "react";
-
 const Expenses = (): JSX.Element => {
-  const { user, authReady } = useContext(AuthContext);
-  const [expenses, setExpenses] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (authReady) {
-      fetch(
-        "/.netlify/functions/expenses",
-        user
-          ? {
-              headers: {
-                Authorization: "Bearer " + user?.token?.access_token,
-              },
-            }
-          : {
-              headers: {
-                Authorization: "Bearer ",
-              },
-            }
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw Error("You must be logged in to view this content");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setExpenses(data);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setExpenses(null);
-        });
-    }
-  }, [user, authReady]);
   return (
     <div>
       <h1>Expenses Detail</h1>
-      <p>{!authReady && <div>Loading...</div>}</p>
-      <p>{error && <div>{error}</div>}</p>
-      <p>
-        {expenses &&
-          expenses?.map((expense: any) => (
-            <div key={expense.name}>{expense.name}</div>
-          ))}
-      </p>
     </div>
   );
 };
