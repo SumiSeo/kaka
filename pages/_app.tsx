b22ca8c6153da17426d6f9a79802f8a876068d6b
Sumi <qkobr94@gmail.com>
2023-02-16 09:04:00 +0100

    feat(graphQL): install graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import "@/styles/base/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/comps/Layout";
 import { AuthContextProvider } from "@/stores/authContext";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
